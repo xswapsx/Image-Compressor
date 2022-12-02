287267a5d48752e0e5e4d1a79becd1a818e92648
swapnil lanjewar <xswapsx@users.noreply.github.com>
2022-12-02 13:26:37 +0530

    init

--- app/src/main/java/com/swapy/imagecompressorex/MainActivity.kt
+++ app/src/main/java/com/swapy/imagecompressorex/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.swapy.imagecompressorex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.swapy.imagecompressor.ImageCompressor
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var imagePath =
+            "/storage/emulated/0/Android/media/com.asdf.mycamerapp/20221202121650702.jpeg"
+        val compressedImagePath = ImageCompressor.compressImage(imagePath)
+
+        Log.d("MainActivity", "onCreate: saved at: $compressedImagePath")
     }
 }
